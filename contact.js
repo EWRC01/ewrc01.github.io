ce3b31dd4fed62eb8706517ee19ff186ae5fa3eb
@Wilfredo <edwin1026@hotmail.es>
2022-10-13 20:17:53 -0600

    Add API Formspree and contact Form

---
+++ contact.js
@@ -0,0 +1,22 @@
+ const form = document.getElementById('formContact');
+
+form.addEventListener('submit', handleSubmit);
+
+async function handleSubmit(e){
+    e.preventDefault();
+    const data = new FormData(this);
+    const response = await fetch(this.action, {
+        method: this.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+
+    if (response.ok) {
+        this.reset()
+        alert('Thanks for contact me :)');
+    }
+}
+
+ 
